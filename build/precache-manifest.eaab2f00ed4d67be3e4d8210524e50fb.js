a335e430b6ae94b420b5e5c6dd7f7af82840b950
Bas Kaal <3askaal@gmail.com>
2020-08-17 00:27:11 +0200

    upload build for github pages

---
+++ build/precache-manifest.eaab2f00ed4d67be3e4d8210524e50fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e01f042dbc649157cc032d032865ca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57c4302bcee38aeb14e4",
+    "url": "/static/css/2.4c487673.chunk.css"
+  },
+  {
+    "revision": "f6c224b6ee95f38b7aaa",
+    "url": "/static/css/main.4f7edaa8.chunk.css"
+  },
+  {
+    "revision": "57c4302bcee38aeb14e4",
+    "url": "/static/js/2.ea0ec59a.chunk.js"
+  },
+  {
+    "revision": "2a2ad68459b1a6f423c112665d485134",
+    "url": "/static/js/2.ea0ec59a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c224b6ee95f38b7aaa",
+    "url": "/static/js/main.032c380d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);

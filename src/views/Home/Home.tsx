b30ca9379608a9124fe6ebb151d6bbe51023ee41
Bas Kaal <3askaal@gmail.com>
2020-09-30 16:18:33 +0200

    content changes

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -17,11 +17,34 @@ export const HomeView = () => {
         <p>
           I'm a Full-stack developer living in Amsterdam, Netherlands.
           Specialized in Front-end but also likes doing Back-end and UX in{' '}
-          <Link to="/projects">side projects</Link>.
+          <Link to="/projects">side projects</Link>. Currently working for for{' '}
+          <a href="https://bloqhouse.com/" target="_blank" rel="noreferrer">
+            Bloqhouse
+          </a>
+          .
         </p>
         <p>
           Tools I'm loving at the moment: NodeJS, Typescript, GraphQL, React,
           Vue, Apollo, Styled Components, lodash
+        </p>
+        <p>
+          Besides building and designing things I love{' '}
+          <a
+            href="https://www.thrashermagazine.com/articles/videos/dime-s-knowing-mixtape-vol-2/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            skateboarding
+          </a>{' '}
+          and{' '}
+          <a
+            href="https://open.spotify.com/album/7aSjaEi3OQ2aZemcJDhMb1?si=mRw5pzU9Q1-rz6cqOpuBRA"
+            target="_blank"
+            rel="noreferrer"
+          >
+            music
+          </a>
+          .
         </p>
       </Body>
       <Space />

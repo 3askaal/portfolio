b00924d9d2d7ae955ab8eaacacf0c02c1293714b
Bas Kaal <3askaal@gmail.com>
2020-10-27 00:29:36 +0100

    layout fixes

--- src/context/MiscContext.tsx
+++ src/context/MiscContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { useLocation, useHistory, Redirect } from 'react-router-dom'
 import { debounce, findIndex } from 'lodash'
-import { PAGES, PROJECTS } from '../constants'
+import { PAGES } from '../constants'
 import { getBlockSize, getContainerHeight, getContainerWidth } from '../helpers'
 
 export const MiscContext = createContext({})
@@ -13,8 +13,8 @@ export const MiscProvider = ({ children }: any) => {
   const [currentPageIndex, setCurrentPageIndex] = useState(0)
   const [previousPageIndex, setPreviousPageIndex] = useState(0)
   const [currentProjectIndex, setCurrentProjectIndex] = useState(0)
+  const [currentLayoutElement, setCurrentLayoutElement] = useState(0)
   const location = useLocation()
-  const history: any = useHistory()
 
   const nextPage = () => {
     setPreviousPageIndex(currentPageIndex)
@@ -27,6 +27,8 @@ export const MiscProvider = ({ children }: any) => {
   }
 
   const updateDimensions = (el?: any) => {
+    setCurrentLayoutElement(el)
+
     const newContainerDimensions: any = {
       width: getContainerWidth(),
       height: getContainerHeight(el),
@@ -34,7 +36,6 @@ export const MiscProvider = ({ children }: any) => {
       amountBlocks: getContainerHeight(el) / getBlockSize()
     }
 
-    console.log('container: ', newContainerDimensions)
     setLayoutDimensions(newContainerDimensions)
   
     const newPaperDimensions: any = {
@@ -43,18 +44,17 @@ export const MiscProvider = ({ children }: any) => {
       blockSize: getBlockSize()
     }
 
-    console.log('paper: ', newPaperDimensions)
     setPaperDimensions(newPaperDimensions)
   }
   
   useEffect(() => {
     setCurrentPageIndex(findIndex(PAGES, { path: location.pathname }))
 
-    // window.addEventListener('resize', debounce(updateDimensions, 200))
+    window.addEventListener('resize', debounce(() => updateDimensions(currentLayoutElement), 200))
 
-    // return () => {
-    //   // window.removeEventListener('resize', debounce(updateDimensions, 200))
-    // }
+    return () => {
+      window.removeEventListener('resize', debounce(() => updateDimensions(currentLayoutElement), 200))
+    }
   }, [])
 
   useEffect(() => {
